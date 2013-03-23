71db0e99b6c229b65b23106ebab8cf6c776626c4
mroesler <micha.roesler@gmail.com>
2013-03-23 15:38:10 +0100

    Alert Test

--- phonegap-workshop-master/js/main.js
+++ phonegap-workshop-master/js/main.js
@@ -1,5 +1,13 @@
 var app = {
 
+	showAlert: function (message, title) {
+		if (navigator.notification) {
+			navigator.notification.alert(message, null, title, 'OK');
+		} else {
+			alert(title ? (title + ": " + message) : message);
+		}
+	},
+
     findByName: function() {
         console.log('findByName');
         this.store.findByName($('.search-key').val(), function(employees) {
@@ -14,7 +22,10 @@ var app = {
     },
 
     initialize: function() {
-        this.store = new WebSqlStore();
+		var self = this;
+        this.store = new WebSqlStore(function() {
+			self.showAlert('Store Initialized', 'Info');
+		});
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
